fb655fdb63fbf68fec55f80176d2d1dbe0ba6666
FrancisVarga <github@varga-net.com>
2012-02-09 01:31:37 +0100

    adding stubbed classes

---
+++ application/php/Application/JsonRpc/V1/App/Service/Game.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: francis
+ * Date: 2/9/12
+ * Time: 1:29 AM
+ * To change this template use File | Settings | File Templates.
+ */
+class Game
+{
+
+}
