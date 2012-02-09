85fd83de656d77717d1c1d5d4a5904ee0d61300d
FrancisVarga <github@varga-net.com>
2012-02-09 02:25:44 +0100

    - cleanup coding style

--- application/php/Application/JsonRpc/V1/App/Service/User.php
+++ application/php/Application/JsonRpc/V1/App/Service/User.php
@@ -2,36 +2,26 @@
 
 namespace Application\JsonRpc\V1\App\Service
 {
-    use Application\Dto\FbBasicDto;
-    
-    use Processus\Abstracts\JsonRpc\AbstractJsonRpcService;
-
-    class User extends AbstractJsonRpcService
+    class User extends \Processus\Abstracts\JsonRpc\AbstractJsonRpcService
     {
 
         /**
          * @return array
          */
-        public function getInitialData ()
+        public function getInitialData()
         {
             // get data from current user
-            $userMvo = $this->getProcessusContext()->getUserBo()->getFacebookUserMvo();
-            
-            $userDto = $userMvo->setDto(new FbBasicDto())->export();
-            
-            // get current user's friends
-            $friendsList = $this->getProcessusContext()->getUserBo()->getAppFriends();
-            
-            $friendsListDto = array();
-            
-            foreach ($friendsList as $friendMvo) {
-                $friendsListDto[] = $friendMvo->setDto(new FbBasicDto())->export();
-            }
-            
-            return array(
-                "user" => $userDto, 
-                "appFriends" => $friendsListDto
-            );
+            $userMvo        = $this->getProcessusContext()->getUserBo()->getFacebookUserMvo();
+            $userDto        = $userMvo->setDto(new \Application\Dto\FbBasicDto())->export();
+            $appFriendsList = $this->getProcessusContext()->getUserBo()->getAppFriends();
+            $friendsIdList  = $this->getProcessusContext()->getFacebookClient()->getFriendsIdList();
+
+            $response                   = array();
+            $response['user']           = $userDto;
+            $response['appFriendsList'] = $appFriendsList;
+            $response['friendsIdList']  = $friendsIdList;
+
+            return $response;
         }
     }
 }
